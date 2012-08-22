8bfd1bc8d18b3ec71d6117c342718f725c5e6fe9
eugene.kotyashov@gmail.com <eugene.kotyashov@gmail.com@bf914c03-4306-6ea0-1b43-ae2c3858b352>
2012-08-22 15:49:03 +0000

    replace vectors with malloc' ed pointers - speed increased

--- trunk/twolevel_fdtd.h
+++ trunk/twolevel_fdtd.h
@@ -1,4 +1,4 @@
-typedef struct two_level_params
+ struct two_level_params
 {
 	double     p1;
 	double    p2;
@@ -12,5 +12,42 @@ typedef struct two_level_params
 	double kp;
 	double ke;
 	double kn;
+};
+
+
+struct FdtdFields
+{
+	 double E;
+	 double H;
+	 double B;
+	 double D;
+	 double P;
+	 double Pold;
+	 double Pold2;
+	 double N;	
+};
+
+
+struct SourceFields
+{
+	double Ein;
+	double Hin;
+};
+
+struct PMLParams
+{
+	double sigma;
+	double k;
+	double a1;
+	double b1;
+	double c1;
+	double d1;
+};
+
+struct MediaParamsIndex
+{
+	int two_level_par_index;
+	int eps_r;
+	int pump;
 };
 
